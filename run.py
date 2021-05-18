4d61a2d14fa2e18df91bb996447a57ce47204a5d
JOOHAN <joohan5071@hanyang.ac.kr>
2021-05-18 15:16:14 +0900

    수정

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-import process
+from Source import process
 import glob
-#Move Folder from "Data" to "Result"
-from distutils.dir_util import  copy_tree
-copy_tree("./Data", "./Result")
+# #Move Folder from "Data" to "Result"
+# from distutils.dir_util import  copy_tree
+# copy_tree("./Data", "./Result")
+
 
 xml=[]
-for filename in glob.glob('./Result/P184640/**/*LMZ*.xml', recursive= True):
+for filename in glob.glob('./Data/P184640/**/*LMZ?*.xml', recursive= True):
     xml.append(filename)
 
 for i in xml:
