0a6980aa82c140cd9e632204b74e36fd0d273ce3
parkseoungmin <psm401@hanyang.ac.kr>
2021-06-03 14:02:51 +0900

    requirements.txt 수정, readme 수정, run 수정, res수정.

--- run.py
+++ run.py
@@ -1,25 +1,29 @@
 from src import process , IVfitting, Measured_Spectra , Processed_spectra , Ref_fitting ,tocsv
-import glob , time
+import glob, time
 from tqdm import tqdm
 
+file_path = '.\dat\P184640\**\*LMZ?.xml'
+save_figure = 'T'
+show_figure = 'T'
+save_csv = 'T'
+
 xml=[]
-for filename in glob.glob('.\dat\P184640\**\*LMZ?.xml', recursive= True):
+for filename in glob.glob(file_path, recursive= True):
     xml.append(filename)
 
 start = time.time()
-xml1 = tqdm(xml)
 
-for i in xml1:
+xml_tqdm = tqdm(xml)
+for i in xml_tqdm:
     filename = i.split('\\')[-1][:-4]
-    xml1.set_description(f'Processing {filename}')
-    # fitting 실행
-    process.fitting(i,'T','F') #빈칸에 save figure 및 show figure를 True or False 로 입력하세요.
+    xml_tqdm.set_description(f'Processing {filename}')
 
+    # fitting 실행
+    process.fitting(i,save_figure,show_figure)
     if int(xml.index(i)) + 1 == len(xml):
-        print("모든 파일이 완료되었습니다 빠이루")
-        break
+        print("모든 파일이 완료되었습니다 수고하셨습니다.")
 
     #csv 실행
-    process.csv_mod(i,'T') # 빈칸에 csv 파일로 저장하고 싶으면 True or False를 입력하세요.
+    process.csv_mod(i,save_csv)
 
 print("실행 시간 :" + str(round(time.time()-start,1))+"초")
